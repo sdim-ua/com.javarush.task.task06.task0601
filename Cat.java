e9a781edba7ff7c8ab9e660a925ee94c899f2bdc
sdim-ua <ingersol@yandex.ua>
2019-02-02 23:13:30 +0400

    Create Cat.java

---
+++ Cat.java
@@ -0,0 +1,14 @@
+/* 
+Метод finalize класса Cat
+*/
+
+public class Cat {
+    //напишите тут ваш код
+     protected void finalize() throws Throwable{
+         
+     }
+    
+    public static void main(String[] args) {
+
+    }
+}
